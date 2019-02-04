d5daa4fcdb33ce41521ef2d85ee745f92d7a4118
Torsten Ostgard <torsten-ostgard@users.noreply.github.com>
2019-02-04 00:35:44 -0600

    Simplify code coverage script

--- ci/coverage.bash
+++ ci/coverage.bash
@@ -2,17 +2,9 @@
 
 set -ex
 
-upload_coverage() {
-    for file in $(ls target/x86_64-unknown-linux-gnu/debug/color_bruteforcer-* | grep -v "\.d"); do
-        mkdir -p "target/cov/$(basename $file)";
-        kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
-    done
+for file in $(ls target/x86_64-unknown-linux-gnu/debug/color_bruteforcer-* | grep -v "\.d"); do
+    mkdir -p "target/cov/$(basename $file)";
+    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
+done
 
-    bash <(curl -s https://codecov.io/bash)
-}
-
-main() {
-    upload_coverage
-}
-
-main
+bash <(curl -s https://codecov.io/bash)
